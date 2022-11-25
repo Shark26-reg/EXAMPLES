e9133eedc2e181f0d2544d3ff220adf0b0a3a512
Roman <shark71@inbox.ru>
2022-11-25 16:14:11 +0300

    Решили 10 пример

---
+++ Example010_Metod/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {1, 12, 31, 18, 15, 16, 17, 18};
+
+int n = array.Length;       // длина массива
+int find = 18;              // ищем число в массиве 
+
+int index = 0;              //  счетчик 
+
+while (index < n)           // условие пока индекс меньши дленны массива
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;                          // если есть одинаковые числа в массиве и нужно найти только одно
+    }
+    //index = index + 1;
+    index++;
+
+}
+
